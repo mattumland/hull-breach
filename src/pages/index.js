22344ed9e56778e4497939819fb73367ebdc2204
Matt Umland <mattumland@gmail.com>
2021-11-21 21:38:31 -0500

    Improve desktop styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,16 +28,22 @@ class IndexPage extends React.Component {
         </a>
 
         <section className="content-section">
-          <h2>HULL BREACH is 200 page hardcover anthology for the Mothership Sci-Fi Horror RPG.</h2>
-          <StaticImage 
-            src="../images/mock-up.png" 
-            alt="Rendering of the Hull Breach book" 
-            placeholder="blurred"  
-          />
+          <h2>A sci-fi horror anthology digest for the Mothership RPG.</h2>
+          <article className="feature">
+            <StaticImage 
+              src="../images/mock-up.png" 
+              alt="Rendering of the Hull Breach book" 
+              placeholder="blurred"  
+              className="rendering"
+              imgClassName="imgRender"
+            />
+            <div className="description">
+              <h3>HULL BREACH is the first hardcover book for the Mothership Sci-Fi Horror RPG. This massive collaboration between 20 independent authors packs dozens of new scenarios, essays, bestiaries, system-hacks, toolkits, and more into over 200 beautifully designed pages. Follow our Kickstarter page for a notification when it goes live in January.</h3>
+            </div>
+          </article> 
           <a className="ks-link" href="https://www.kickstarter.com/projects/yusem/hull-breach-vol-1">
-            <h3>Follow us on Kickstarter</h3>
+            <h2>Follow us on Kickstarter</h2>
           </a>
-          <p>HULL BREACH is the first hardcover book for the Mothership Sci-Fi Horror RPG. This massive collaboration between 20 independent authors packs dozens of new scenarios, essays, bestiaries, system-hacks, toolkits, and more into over 200 beautifully designed pages. Follow our Kickstarter page for a notification when it goes live in January.</p>
         </section>
 
         <button className="ad" onClick= { this.toggleBanner }>
