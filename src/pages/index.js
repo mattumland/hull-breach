cfe71cec0cd4030a169b212f46cd98d83009814a
Matt Umland <mattumland@gmail.com>
2021-12-05 15:42:49 -0500

    Improve responsive styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,69 +19,73 @@ class IndexPage extends React.Component {
       <main>
         <title>HULL BREACH</title>
         <section className="banner-section">
-          {/* <StaticImage 
+          <StaticImage 
             src="../images/hullbreach-cover-13000px300dpi-20211027.jpeg" 
             alt="Debris being pulled through a hole in the hull of a ship" 
             placeholder="blurred"  
             className="banner-image"
-          /> */}
-          <h1 className="hidden">HULL BREACH</h1>
-          <StaticImage 
-              src="../images/hull-breach-title-01.png" 
-              alt="Hull Breach" 
-              placeholder="blurred" 
-              className="banner-title" 
           />
-          <StaticImage 
-              src="../images/hb-subhead-01.png" 
-              alt="A sci-fi horror ttrpg anthology" 
-              placeholder="blurred" 
-              className="banner-subtitle" 
-          />
-          <div className="banner-logo">
-            <StaticImage 
-              src="../images/mosh-logo.png" 
-              alt="Logo for the Mothership Sci-Fi Horror RPG" 
-              placeholder="blurred"  
-              className="logo"
-            />
-          </div>
-          <aside className="banner-links">
-            <div>
-              <a href="https://ianyusem.substack.com/p/anodyne-direct-00-boot-sequence?showWelcome=true">
-                <p className="link-text">Join the newsletter</p>
-              </a>
+          <div className="banner-content">
+            <div className="title-box">
+              <h1>
+               <StaticImage 
+                  src="../images/hull-breach-title-01.png" 
+                  alt="HULL BREACH" 
+                  placeholder="blurred" 
+                  className="banner-title" 
+              />
+              </h1>
+              <StaticImage 
+                  src="../images/hb-subhead-01.png" 
+                  alt="A sci-fi horror ttrpg anthology" 
+                  placeholder="blurred" 
+                  className="banner-subtitle" 
+              />
+              <div className="banner-logo">
+                <StaticImage 
+                  src="../images/mosh-logo.png" 
+                  alt="For use with the Mothership Sci-Fi Horror RPG" 
+                  placeholder="blurred"  
+                  className="logo"
+                />
+              </div>
+            </div>
+            <div className="banner-links">
               <a href="https://www.kickstarter.com/projects/yusem/hull-breach-vol-1">
                 <p className="link-text" >Follow the Kickstarter</p>
               </a>        
+              <a href="https://ianyusem.substack.com/p/anodyne-direct-00-boot-sequence?showWelcome=true">
+                <p className="link-text">Join the newsletter</p>
+              </a>
             </div>
-          </aside>
+          </div>
         </section>
         <section className="content-section">
           <article className="feature">
-            <div className="imageContainer">
-              <StaticImage 
-                src="../images/HardcoverUVTestFront.png" 
-                alt="Rendering of the Hull Breach book" 
-                placeholder="dominantColor"  
-                className="rendering"
-              />
-            </div>
+            <StaticImage 
+              src="../images/HardcoverUVTestFront.png" 
+              alt="Artist rendering of Hull Breach book cover" 
+              placeholder="dominantColor"  
+              className="rendering"
+            />
             <div className="description">
-              <p>HULL BREACH is the first hardcover book for the Mothership Sci-Fi Horror RPG. This massive collaboration between 20 independent authors packs dozens of new scenarios, essays, bestiaries, system-hacks, toolkits, and more into over 200 beautifully designed pages. Follow our Kickstarter page for a notification when it goes live in January.</p>
+              <p>HULL BREACH is the first hardcover book for the Mothership Sci-Fi Horror RPG. This massive collaboration between 20 independent authors packs dozens of new scenarios, essays, bestiaries, system-hacks, toolkits, and more into over 200 beautifully designed pages.</p>
+              <a href="https://www.kickstarter.com/projects/yusem/hull-breach-vol-1">
+                <p className="description-link">Learn more at Kickstarter</p>
+              </a>  
             </div>
           </article> 
         </section>
         <section className="spread-section">
           <StaticImage 
             src="../images/open_mockup1.png" 
-            alt="Rendering of spread from the Hull Breach book" 
+            alt="Artist rendering of interior pages of Xeiram article from Hull Breach" 
             placeholder="dominantColor"  
             className="rendering"
           />
           <StaticImage 
             src="../images/open_mockup2.png" 
-            alt="Rendering of spread from the Hull Breach book" 
+            alt="Artist rendering of interior pages of Most Wanted article from Hull Breach" 
             placeholder="dominantColor"  
             className="rendering"
           />
@@ -93,7 +97,7 @@ class IndexPage extends React.Component {
                 <StaticImage 
                   src="../images/banner.png"  
                   alt="AD - click here if you or a loved one where injured in the Hodder Forest Reserve Habitat accident."
-                  className="flicker desktop"
+                  className="desktop"
                 />
               </div>
             }
@@ -102,7 +106,7 @@ class IndexPage extends React.Component {
                 <StaticImage 
                   src="../images/arg-banner_mobile.png"  
                   alt="AD - click here if you or a loved one where injured in the Hodder Forest Reserve Habitat accident."
-                  className="flicker mobile"
+                  className="mobile"
                 />
               </div>
             }
